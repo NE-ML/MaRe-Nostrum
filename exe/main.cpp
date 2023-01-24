22948c02f02ecac3f5c8f6c420a8915cddb560bc
Лев Барабанщиков <levbara1@gmail.com>
2023-01-24 18:42:46 +0300

    Changed wrapper program

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,22 +1,34 @@
 #include <thread>
+#include <iostream>
+#include <filesystem>
 #include "map_reduce.h"
 #include "mapper.h"
 #include "reducer.h"
 
 using namespace mare_nostrum;
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <input_file>" << std::endl;
+        return 1;
+    }
     MapReduce obj;
-    obj.setInputFiles("../data/file10000.txt");
-    obj.setMaxSimultaneousWorkers(std::thread::hardware_concurrency());
-    obj.setNumReducers(4);
-    obj.setTmpDir("../tmp/");
-    std::function<std::vector<std::pair<std::string, int>>(const std::string &)> mapper = Mapper();
-    obj.setMapper(mapper);
-    std::function<std::vector<std::pair<std::string, int>>
-                    (const std::vector<std::pair<std::string, std::vector<int>>> &)> reducer = Reducer();
-    obj.setReducer(reducer);
-    obj.start();
+
+    try {
+        obj.setInputFiles("data/" + std::string(argv[1]));
+        obj.setMaxSimultaneousWorkers(std::thread::hardware_concurrency());
+        obj.setNumReducers(4);
+        obj.setTmpDir("../tmp/");
+        std::function<std::vector<std::pair<std::string, int>>(const std::string &)> mapper = Mapper();
+        obj.setMapper(mapper);
+        std::function<std::vector<std::pair<std::string, int>>
+                (const std::vector<std::pair<std::string, std::vector<int>>> &)> reducer = Reducer();
+        obj.setReducer(reducer);
+        obj.start();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
