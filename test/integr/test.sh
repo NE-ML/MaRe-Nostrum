b10754f982b634968eae8c227416ec3e81c212d1
Лев Барабанщиков <levbara1@gmail.com>
2023-01-24 18:59:21 +0300

    Added integration tests

---
+++ test/integr/test.sh
@@ -0,0 +1,27 @@
+#!usr/bin/env bash
+
+# Set up the binaries
+mkdir -p "build"
+cd "build"
+cmake .. > /dev/null
+make > /dev/null
+cd ..
+
+path="./build/MaRe-Nostrum"
+
+output=$($path "file10000.txt")
+if [[ $? != 0 ]]; then
+  echo "FAILED"
+else
+  # Check the output
+  # It must contain 10 '-' and 10 words "about:". sum of numbers following "about:" must be 196
+  if [[ $(echo $output | grep -o "-" | wc -l) != 10 ]]; then
+    echo "FAILED"
+  elif [[ $(echo $output | grep -o "about:" | wc -l) != 10 ]]; then
+    echo "FAILED"
+  elif [[ $(echo $output | grep -o "about: [0-9]*" | grep -o "[0-9]*" | awk '{s+=$1} END {print s}') != 196 ]]; then
+    echo "FAILED"
+  else
+    echo "OK"
+  fi
+fi
